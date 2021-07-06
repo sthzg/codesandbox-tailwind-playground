96834ccb3f4314186c16ac41cbe82081c31c3530
Stephan Herzog <sthzgvie@gmail.com>
2021-07-06 09:06:01 +0200

    chore: fix spelling

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -7,11 +7,10 @@ const COMMON_HOSTNAME = 'sse.codesandbox.io';
 const isSSE = os.hostname().includes('sse-sandbox');
 
 /**
- * The resolved hostname allows HMR on Codesandbox to  establish a websocket connection.
+ * The fully qualified hostname allows HMR on Codesandbox to  establish a websocket connection.
  *
  * This is a (currently) working but brittle attempt to build the subdomain
- * of the sandbox. TODO: is there a more official ay to gather this infomration?
-
+ * of the sandbox. TODO: is there a more official way to gather this information?
  */
 const getHostName = () => {
   if (!isSSE) {
